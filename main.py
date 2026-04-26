52500a73141512c811df910f677ac25f05becd31
Andrew Izatt <izatt.andrew@gmail.com>
2026-04-26 12:55:47 -0600

    Add tkinter cookbook

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -11,8 +12,40 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    countdown()
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def countdown(count):
+    canvas.itemconfig(timer_text, text=count)
+    if count > 0:
+        # window.after(ms=1000, func=countdown, count - 1)
+        window.after( 1000, countdown, count - 1)
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro")
+window.config(padx=100, pady=50, bg=YELLOW)
+
+main_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+main_label.grid(row=0, column=1)
+check_label = Label(text="✓", fg=GREEN, bg=YELLOW)
+check_label.grid(row=3, column=1)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file="tomato.png")
+canvas.create_image(100, 112, image=tomato_img)
+timer_text = canvas.create_text(100, 130, text="00:00", fill="white", font=(FONT_NAME, 35, "bold"))
+canvas.grid(row=1, column=1)
+
+# Must be below canvas object
+countdown(5)
+
+start_button = Button(text="Start", highlightthickness=0, highlightbackground=YELLOW)
+start_button.grid(row=2, column=0)
+
+reset_button = Button(text="Reset", highlightthickness=0, highlightbackground=YELLOW)
+reset_button.grid(row=2, column=2)
+
+window.mainloop()
